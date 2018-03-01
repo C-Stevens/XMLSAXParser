4da07e57ad5f33feb394a7cc0f1fafa705a06d4e
Colin Stevens <mail@colinjstevens.com>
2018-02-28 22:58:20 -0600

    Initial with file chooser and new UI

--- src/xmlsaxparser/XMLSAXParserFXMLController.java
+++ src/xmlsaxparser/XMLSAXParserFXMLController.java
@@ -5,31 +5,71 @@
  */
 package xmlsaxparser;
 
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Label;
+import javafx.scene.control.TreeView;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 /**
  *
  * @author Colin
  */
 public class XMLSAXParserFXMLController implements Initializable {
+
+    private final FileChooser fileChooser = new FileChooser();
+    private Stage stage;
     
     @FXML
-    private Label label;
+    private TreeView tree;
     
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private void handleFileOpen(ActionEvent event) {
+        System.out.println("File open event"); //DEBUG
+        File inputFile = fileChooser.showOpenDialog(stage);
+        if(inputFile != null) {
+            openFile(inputFile);
+        } else {
+            //TODO error handling
+        }
     }
     
+    @FXML
+    private void handleAbout(ActionEvent event) {
+        System.out.println("About event"); //DEBUG
+        Alert alert = new Alert(AlertType.INFORMATION);
+        alert.setTitle("About");
+        alert.setHeaderText(null);
+        alert.setContentText("This application was written to demonstrate generic XML parsing and display abilities in Java using the SAX parser.\n\nThis application was written by Colin Stevens for MU-CS4330: OO & Software Design II");
+        alert.showAndWait();
+    }
+   
     @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+    public void initialize(URL url, ResourceBundle rb) {} // Unused
+        
+    public void start(Stage stage) {
+        this.stage = stage;
+        fileChooser.getExtensionFilters().add(
+                new FileChooser.ExtensionFilter("XML Files", "*.xml")
+        );
+        stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+            public void handle(WindowEvent we) {
+                // Do some closing of vars here... //TODO
+            }
+        });
+    }
+    
+    private void openFile(File file) {
+        //TODO
+    }
     
 }
